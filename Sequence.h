ec42511e82e77586c91bebe1d0500cf8b7db0acd
andreytolstov2007 <andreytolstov2007@gmail.com>
2026-05-28 12:40:45 +0300

    Update Sequence.h
    в 1 строчку

--- Sequence.h
+++ Sequence.h
@@ -1,59 +1,53 @@
-#ifndef Sequence_H
-#define Sequence_H
+#ifndef Option_H
+#define Option_H
 
-#include "Option.h"
-#include "IEnumerator.h"
+#include "Errors.h"
 
 template <typename T>
-class Sequence : public IEnumerable<T> {
+class Option {
+private:
+    bool has_value;
+    T value;
+
 public:
-    virtual ~Sequence() {}
-
-    virtual T Get_first() const = 0;
-    virtual T Get_last() const = 0;
-    virtual T Get(long index) const = 0;
-    virtual Sequence<T>* Get_subsequence(long start_index, long end_index) const = 0;
-    virtual long Get_length() const = 0;
+    Option() { has_value = false; }
     
-    virtual Sequence<T>* Append(T item) = 0;
-    virtual Sequence<T>* Prepend(T item) = 0;
-    virtual Sequence<T>* Insert_at(T item, long index) = 0;
-    virtual Sequence<T>* Concat(Sequence<T> *other) = 0;
-    
-    virtual Sequence<T>* Map(T (*function)(T)) const = 0;
-    virtual Sequence<T>* Where(bool (*predicate)(T)) const = 0;
-    virtual T Reduce(T (*function)(T, T), T init) const = 0;
-    
-    virtual Option<T> Try_get_first(bool (*predicate)(T)) const = 0;
-    virtual Option<T> Try_get_last(bool (*predicate)(T)) const = 0;
-    
-    virtual Sequence<T>* Slice(long start, long count, Sequence<T> *insert_seq = nullptr) = 0;
-    virtual Sequence<T>* Zip(Sequence<T> *other) const = 0;
-    virtual Sequence<T>* Flat_map(Sequence<T>* (*function)(T)) const = 0;
-    
-    virtual Sequence<T>* Clone() const = 0;
-
-    T operator[](long index) const {
-        return Get(index);
+    explicit Option(const T &val) {
+        has_value = true;
+        value = val;
     }
+    
+    bool Is_some() const {  return has_value; }
 
-    virtual bool operator==(const Sequence<T> &other) const {
-        if (Get_length() != other.Get_length()) {
-            return false;
-        }
-        for (long sequence_index = 0; sequence_index < Get_length(); sequence_index ++) {
-            if (Get(sequence_index) != other.Get(sequence_index)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    bool operator!=(const Sequence<T> &other) const {
-        if (*this == other) {
+    bool Is_none() const { 
+        if (has_value == true) {
             return false;
         }
         return true;
+    }
+    
+    T Get() const {
+        if (has_value == false) {
+            throw EmptyCollection();
+        }
+        return value;
+    }
+    
+    T Get_or_default(const T &defaultValue) const {
+        if (has_value == true) {
+            return value;
+        } else {
+            return defaultValue;
+        }
+    }
+    
+    template <typename ResultType>
+    Option<ResultType> Map(ResultType (*function)(T)) const {
+        if (has_value == true) {
+            return Option<ResultType>(function(value));
+        } else {
+            return Option<ResultType>();
+        }
     }
 };
-#endif //Sequence_H
+#endif //Option_H
